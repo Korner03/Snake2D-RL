a875fc3b8cb79db8da7dc9d0bc77c00efd7e06c8
Nadav <nadavc220@gmail.com>
2020-02-20 15:04:36 +0200

    First try for full implementation of linear model. the board representation is a vector which denotes the minimal distance for every element type on board.

--- policies/linear_policy.py
+++ policies/linear_policy.py
@@ -59,8 +59,11 @@ class Linear(bp.Policy):
         """
         min_pos_vec = [np.inf] * 11
         curr_distance = 0
-        curr_points_batch = None
-        board_iter = pos_by_distance_iter(state, action, self.max_radius)
+        pos, _ = state[1]
+        board_size = pos.board_size
+
+        initial_pos = self.get_next_position(state, action)
+        board_iter = self.pos_by_distance_iter(initial_pos, board_size, self.max_radius)
 
         while True:
             curr_points_batch = next(board_iter)
@@ -74,7 +77,29 @@ class Linear(bp.Policy):
             curr_distance += 1
 
         assert np.inf not in min_pos_vec
-        return curr_points_batch
+        return min_pos_vec
+
+    def get_pos_neighbors(self, curr_positions, board_size):
+        new_positions = set()
+        for pos in curr_positions:
+            x, y = pos
+            r = (x, (y + 1) % board_size[1])
+            l = (x, (y - 1) % board_size[1])
+            u = ((x - 1) % board_size[0], y)
+            d = ((x + 1) % board_size[0], y)
+            new_positions.union({r, l, u, d})
+        return new_positions
+
+    def pos_by_distance_iter(self, initial_pos, board_size, limit=20):
+        checked_positions = set()
+        curr_positions = [initial_pos]
+
+        for i in range(limit):
+            yield curr_positions
+            checked_positions.union(curr_positions)
+            curr_positions = self.get_pos_neighbors(curr_positions, board_size).difference(checked_positions)  # discard visited locations
+            curr_positions = list(curr_positions)
+        yield None
 
     def calculate_best_action(self, state):
         """
